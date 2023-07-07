781296e7ef3248398248f743ec83a312032287fc
Abaljerind <abaljerinda@gmail.com>
2023-07-08 05:45:21 +0700

    add colors + fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        desaturatedRed: "hsl(0, 36%, 70%)",
+        softRed: "hsl(0, 93%, 68%)",
+        darkGrayishRed: "hsl(0, 6%, 24%)",
+      },
+      fontFamily: {
+        josefin: "Josefin Sans",
+      },
+    },
   },
   plugins: [],
 };
