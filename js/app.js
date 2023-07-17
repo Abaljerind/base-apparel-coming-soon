431a8c1b76724cda279a703189b03b79bfa620e4
Abaljerind <abaljerinda@gmail.com>
2023-07-17 12:00:52 +0700

    add xl width to input email section

--- js/app.js
+++ js/app.js
@@ -8,12 +8,14 @@ const emailValidation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 const timeout = setInterval(() => {
   errorMsg.style.display = 'none';
   errorAlert.style.display = 'none';
+  inputField.style.borderColor = 'hsl(0, 36%, 70%)';
 }, 3000);
 
 arrowBtn.addEventListener('click', function() {
   if (inputField.value.length === 0 || !inputField.value.match(emailValidation)) {
     errorMsg.style.display = 'inline';
     errorAlert.style.display = 'inline';
+    inputField.style.borderColor = 'hsl(0, 93%, 68%)';
     errorMsg.textContent = 'Please provide a valid email';
     return timeout;
   }
